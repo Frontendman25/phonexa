b1f6e72ccca5e66f28830b89acc0fec7a7145aff
Frontendman25 <designer24@i.ua>
2020-01-05 15:22:42 +0200

    fix select errors

--- js/script.js
+++ js/script.js
@@ -1,55 +1,55 @@
-"use strict";
+'use strict'
 
 $(function() {
-  var wrapper = document.querySelector(".wrapper");
-  var formRegistration = document.querySelector("#formRegistration");
-  var formSpecalization = document.querySelector("#formSpecalization");
-  var checkInfo = document.querySelector(".check-info");
-  var editBtn = document.querySelector(".check-info__btn_edit");
-  var sendBtn = document.querySelector(".check-info__btn_send");
-  var thanks = document.querySelector(".thanks");
-  var dataFromForms = {};
-  $(".departments").selectric();
-  $(".vacancy").selectric(); // Animate disappeareance block
+  var wrapper = document.querySelector('.wrapper')
+  var formRegistration = document.querySelector('#formRegistration')
+  var formSpecalization = document.querySelector('#formSpecalization')
+  var checkInfo = document.querySelector('.check-info')
+  var editBtn = document.querySelector('.check-info__btn_edit')
+  var sendBtn = document.querySelector('.check-info__btn_send')
+  var thanks = document.querySelector('.thanks')
+  var dataFromForms = {}
+  $('.departments').selectric()
+  $('.vacancy').selectric() // Animate disappeareance block
 
   var toggleAppeareance = function toggleAppeareance(element, appeareance) {
-    if (appeareance === "disappear") {
-      wrapper.style.overflow = "hidden";
-      element.classList.remove("animateAppear");
-      element.classList.add("animateDisappear");
+    if (appeareance === 'disappear') {
+      wrapper.style.overflow = 'hidden'
+      element.classList.remove('animateAppear')
+      element.classList.add('animateDisappear')
       setTimeout(function() {
-        element.style.display = "none";
-      }, 500);
-      return;
-    } else if (appeareance === "appear") {
-      element.classList.remove("animateDisappear");
-      element.classList.add("animateAppear");
+        element.style.display = 'none'
+      }, 500)
+      return
+    } else if (appeareance === 'appear') {
+      element.classList.remove('animateDisappear')
+      element.classList.add('animateAppear')
       setTimeout(function() {
-        element.style.display = "block";
-      }, 500);
+        element.style.display = 'block'
+      }, 500)
       setTimeout(function() {
-        wrapper.style.overflow = "auto";
-      }, 1000);
-      return;
+        wrapper.style.overflow = 'auto'
+      }, 1000)
+      return
     }
-  }; // Validate formRegistration
+  } // Validate formRegistration
 
   $.validator.addMethod(
-    "regex",
+    'regex',
     function(value, element, param) {
-      return this.optional(element) || param.test(value);
+      return this.optional(element) || param.test(value)
     },
-    "Letters only"
-  );
-  $("#formRegistration").validate({
+    'Letters only'
+  )
+  $('#formRegistration').validate({
     submitHandler: function submitHandler(form, e) {
-      e.preventDefault ? e.preventDefault() : (e.returnValue = false);
-      var serializedArray = $(form).serializeArray();
+      e.preventDefault ? e.preventDefault() : (e.returnValue = false)
+      var serializedArray = $(form).serializeArray()
       $.map(serializedArray, function(n, i) {
-        dataFromForms[n["name"]] = n["value"];
-      });
-      toggleAppeareance(form, "disappear");
-      toggleAppeareance(formSpecalization, "appear");
+        dataFromForms[n['name']] = n['value']
+      })
+      toggleAppeareance(form, 'disappear')
+      toggleAppeareance(formSpecalization, 'appear')
     },
     rules: {
       name: {
@@ -73,19 +73,19 @@ $(function() {
       },
       password_confirmation: {
         required: true,
-        equalTo: "#password"
+        equalTo: '#password'
       }
     },
     messages: {
       password:
-        "Required at least one number (0-9), uppercase and lowercase letters (a-Z), and at least one special character (!@#$%^&*~)",
-      password_confirmation: "Must be equal to password"
+        'Required at least one number (0-9), uppercase and lowercase letters (a-Z), and at least one special character (!@#$%^&*~)',
+      password_confirmation: 'Must be equal to password'
     }
-  }); // Work whith <select></select>
+  }) // Work whith <select></select>
 
-  var departments = document.querySelector(".departments");
-  var vacancy = document.querySelector(".vacancy");
-  var isGetData = false;
+  var departments = document.querySelector('.departments')
+  var vacancy = document.querySelector('.vacancy')
+  var isGetData = false
 
   var fillCheckInfoBlock = function fillCheckInfoBlock() {
     var name = dataFromForms.name,
@@ -93,7 +93,7 @@ $(function() {
       email = dataFromForms.email,
       company_name = dataFromForms.company_name,
       departments = dataFromForms.departments,
-      vacancy = dataFromForms.vacancy;
+      vacancy = dataFromForms.vacancy
     return '\n      <h3 class="check-info__caption">Name: <span class="check-info__value">'
       .concat(
         name,
@@ -115,88 +115,84 @@ $(function() {
         departments,
         '</span>\n      </h3>\n      <h3 class="check-info__caption">Job Title: <span class="check-info__value">'
       )
-      .concat(vacancy, "</span>\n      </h3>\n    ");
-  }; // departments.addEventListener('click', function(e) {
+      .concat(vacancy, '</span>\n      </h3>\n    ')
+  } // departments.addEventListener('click', function(e) {
 
-  $(".departments").on("selectric-before-open", function(e) {
+  $('.departments').on('selectric-before-open', function(e) {
     if (!isGetData) {
-      console.log("selectctric-before-open");
+      $('#departmentsError, #vacancyError').css({ display: 'none' })
 
-      var _departments = Object.keys(data.departments);
+      var _departments = Object.keys(data.departments)
 
-      var options = "";
+      var options = ''
 
       _departments.forEach(function(key) {
-        options += "<option>".concat(key, "</option>");
-      });
+        options += '<option>'.concat(key, '</option>')
+      })
 
-      this.innerHTML = options;
-      isGetData = true;
+      this.innerHTML = options
+      isGetData = true
       var _data = data,
-        departmentsData = _data.departments;
-      var selected = $(".departments")
-        .children("option:selected")
-        .text();
-      var currentVacancy = departmentsData[selected];
-      var optionsData = "";
-      vacancy.disabled = false;
+        departmentsData = _data.departments
+      var selected = $('.departments')
+        .children('option:selected')
+        .text()
+      var currentVacancy = departmentsData[selected]
+      var optionsData = ''
+      vacancy.disabled = false
       currentVacancy.forEach(function(el) {
-        optionsData += "<option>".concat(el, "</option>");
-      });
-      vacancy.innerHTML = optionsData;
-      $(".departments")
+        optionsData += '<option>'.concat(el, '</option>')
+      })
+      vacancy.innerHTML = optionsData
+      $('.departments')
         .change()
-        .selectric("refresh");
-      $(".vacancy")
+        .selectric('refresh')
+      $('.vacancy')
         .change()
-        .selectric("refresh");
+        .selectric('refresh')
     }
-  });
-  $(".departments").on("selectric-change", function() {
-    console.log("selectctric-before-change");
+  })
+  $('.departments').on('selectric-change', function() {
     var _data2 = data,
-      departments = _data2.departments;
+      departments = _data2.departments
     var selected = $(this)
-      .children("option:selected")
-      .text();
-    var currentVacancy = departments[selected];
-    var options = "";
-    vacancy.disabled = false;
+      .children('option:selected')
+      .text()
+    var currentVacancy = departments[selected]
+    var options = ''
+    vacancy.disabled = false
     currentVacancy.forEach(function(el) {
-      options += "<option>".concat(el, "</option>");
-    });
-    vacancy.innerHTML = options;
-    $(".vacancy")
-      .change()
-      .selectric("refresh");
-  }); // Edit button click
+      options += '<option>'.concat(el, '</option>')
+    })
+    vacancy.innerHTML = options
+  }) // Edit button click
 
-  editBtn.addEventListener("click", function(e) {
-    toggleAppeareance(checkInfo, "disappear");
-    toggleAppeareance(formRegistration, "appear");
-  }); // Send button click
+  editBtn.addEventListener('click', function(e) {
+    toggleAppeareance(checkInfo, 'disappear')
+    toggleAppeareance(formRegistration, 'appear')
+  }) // Send button click
 
-  sendBtn.addEventListener("click", function(e) {
-    localStorage.setItem("infoFromForms", JSON.stringify(dataFromForms));
-    toggleAppeareance(checkInfo, "disappear");
-    toggleAppeareance(thanks, "appear");
-    formRegistration.reset();
-    formSpecalization.querySelector(".form__field_select").innerHTML =
-      '\n      <select class="departments" name="departments">\n        <option selected disabled>Departments</option>\n      </select>\n      <select class="vacancy" name="vacancy" disabled>\n        <option selected disabled>Vacancy</option>\n      </select>\n    ';
-  }); // Validate formSpecalization
+  sendBtn.addEventListener('click', function(e) {
+    localStorage.setItem('infoFromForms', JSON.stringify(dataFromForms))
+    toggleAppeareance(checkInfo, 'disappear')
+    toggleAppeareance(thanks, 'appear')
+    formRegistration.reset()
+    formSpecalization.querySelector('.form__field_select').innerHTML =
+      '\n      <select class="departments" name="departments">\n        <option selected disabled>Departments</option>\n      </select>\n      <select class="vacancy" name="vacancy" disabled>\n        <option selected disabled>Vacancy</option>\n      </select>\n    '
+  }) // Validate formSpecalization
 
-  $("#formSpecalization").validate({
+  $('#formSpecalization').validate({
     submitHandler: function submitHandler(form, e) {
-      e.preventDefault();
-      var serializedArray = $(form).serializeArray();
+      e.preventDefault()
+      var serializedArray = $(form).serializeArray()
       $.map(serializedArray, function(n, i) {
-        dataFromForms[n["name"]] = n["value"];
-      });
+        dataFromForms[n['name']] = n['value']
+      })
       checkInfo.querySelector(
-        ".check-info__info"
-      ).innerHTML = fillCheckInfoBlock();
-      toggleAppeareance(form, "disappear");
-      toggleAppeareance(checkInfo, "appear");
+        '.check-info__info'
+      ).innerHTML = fillCheckInfoBlock()
+      toggleAppeareance(form, 'disappear')
+      toggleAppeareance(checkInfo, 'appear')
     },
     rules: {
       departments: {
@@ -206,5 +202,14 @@ $(function() {
         required: true
       }
     }
-  });
-});
+  })
+
+  $('.formSpecalizationBtn').on('click', function(e) {
+    if (!$('#formSpecalization').valid()) {
+      e.preventDefault()
+      $('#departmentsError, #vacancyError').css({ display: 'block' })
+    } else {
+      $('#departmentsError, #vacancyError').css({ display: 'none' })
+    }
+  })
+})
